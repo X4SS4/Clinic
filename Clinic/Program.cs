4caa90a96ec8d241faae381245e8c5239ba5e388
ZABİL <xas.zab.kam@gmail.com>
2023-12-08 00:48:55 +0400

    Create http listener

--- Clinic/Program.cs
+++ Clinic/Program.cs
@@ -1,1 +1,40 @@
-﻿
+﻿using System.Net;
+
+HttpListener listener = new HttpListener();
+
+listener.Prefixes.Add("http://*:8080/");
+
+listener.Start();
+
+Console.WriteLine("App started");
+
+while (true)
+{
+    var context = await listener.GetContextAsync();
+    using var writer = new StreamWriter(context.Response.OutputStream);
+    Console.WriteLine("p1");
+
+
+    var endpoint = context.Request.RawUrl.Split("/", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+
+
+    if (endpoint == null || endpoint.Length == 0)
+    {
+        var homepage = await File.ReadAllTextAsync("Views/Home.html");
+        await writer.WriteLineAsync(homepage);
+        context.Response.StatusCode = (int)HttpStatusCode.OK;
+        context.Response.ContentType = "text/html";
+        continue;
+    }
+
+
+
+    switch (endpoint[0].ToLower())
+    {
+        case "staff":
+            break;
+    }
+}
+
+Console.WriteLine("App ended");
