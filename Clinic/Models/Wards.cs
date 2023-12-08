c3c42afe4dd5e67e2516fbdc618b65196683c98d
ZABİL <xas.zab.kam@gmail.com>
2023-12-08 14:48:42 +0400

    Update Patients and Wards

--- Clinic/Models/Wards.cs
+++ Clinic/Models/Wards.cs
@@ -1,6 +1,18 @@
-public class Ward
+public class Room
 {
     public int Id { get; set; }
     public int Number { get; set; }
+    public int Floor { get; set; }
+}
+
+public class Cabinet : Room
+{
+    public Staff owner { get; set; }
+}
+
+public class Ward : Room
+{
+    public int Number { get; set; }
+    public int PatientCapacity { get; set; }
     public bool IsReanimation { get; set; }
 }
