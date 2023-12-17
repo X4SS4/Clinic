8f5362503363887cf0a1c721d1f921fd42e96c25
ZABİL <xas.zab.kam@gmail.com>
2023-12-18 00:59:01 +0400

    Add HttpListener and HomePage

---
+++ Clinic_App/Controllers/ClinicControllers/HomePage/HomePageController.cs
@@ -0,0 +1,7 @@
+namespace Clinic_App.Controllers.ClinicControllers.HomePage;
+using Clinic_App.Controllers.BaseControllers;
+
+public class HomePageController : BaseController
+{
+    
+}
--- Clinic_App/Program.cs
+++ Clinic_App/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+
+HttpListener httpListener = new HttpListener();
+httpListener.Prefixes.Add("http://*:8080/");
+httpListener.Start();
+
+while (true)
+{
+    var context = await httpListener.GetContextAsync();
+    using var writer = new StreamWriter(context.Response.OutputStream);
+
+    var pageHtml = await File.ReadAllTextAsync("Views/HomePage.html");
+    await writer.WriteLineAsync(pageHtml);
+    context.Response.StatusCode = (int)HttpStatusCode.OK;
+    context.Response.ContentType = "text/html";
+
+}
