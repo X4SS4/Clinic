d961c55277c283c95d8db5ab1439ad1abdd028e1
Хасайлы Забиль Камиль <Hasay_tf23@itstep.edu.az>
2024-02-02 20:24:23 +0400

    Initial/Reset project commit

--- Clinic_App/Controllers/BaseControllers/BaseController.cs
+++
@@ -1,9 +0,0 @@
-namespace Clinic_App.Controllers.BaseControllers;
-using Clinic_App.Clinic_db_ef;
-
-public class BaseController
-{
-    protected ClinicDbContext clinicDbContext = new();
-
-    public object HttpContext { get; internal set; }
-}
--- Clinic_App/Models/Rooms/Cabinet.cs
+++
@@ -1,8 +0,0 @@
-namespace Clinic_App.Models.Rooms;
-using Clinic_App.Models.BaseModels;
-using Clinic_App.Models.Persons;
-
-public class Cabinet : Room
-{
-    public Staff? owner { get; set; }
-}
