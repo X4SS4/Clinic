1e9ad432a2c647adb6394469b0845704c3dc02a8
ZABİL <xas.zab.kam@gmail.com>
2024-03-26 17:30:25 +0400

    Add image path

--- ClinicApp/ClinicApp.Core/Employees/Entities/Employee.cs
+++ ClinicApp/ClinicApp.Core/Employees/Entities/Employee.cs
@@ -8,5 +8,6 @@ public class Employee : IdentityUser<int>
     public string? FIN { get; set; }
     public string? Firstname { get; set; }
     public string? Lastname { get; set; }
+    public string? ImagePath { get; set; }
     public MedicalDepartmentsEnum? MedicalDepartment { get; set; }
 }
