16304b98faa8738e0c24a0a79f58f4f86baf47bd
ZABİL <xas.zab.kam@gmail.com>
2024-03-26 17:34:18 +0400

    Clean up

--- Clinic_App/Attributes/HttpGetAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace Clinic_App.Attributes;
-using Clinic_App.Attributes.BaseAttribute;
-
-[AttributeUsage(AttributeTargets.Method)]
-public class HttpGetAttribute : HttpMethodAttribute
-{
-    public HttpGetAttribute(string path) : base(path, "GET") { }
-}
--- Clinic_App/Attributes/HttpPostAttribute.cs
+++
@@ -1,8 +0,0 @@
-namespace Clinic_App.Attributes;
-using Clinic_App.Attributes.BaseAttribute;
-
-[AttributeUsage(AttributeTargets.Method)]
-public class HttpPostAttribute : HttpMethodAttribute
-{
-    public HttpPostAttribute(string path) : base(path, "POST") { }
-}
